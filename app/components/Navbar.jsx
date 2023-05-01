4d9dca93675117829202d439155dd63b51517d0d
ajikia15 <ajikia15@gmail.com>
2023-05-01 23:59:17 +0400

    Restart

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>aqane navigacia</h1>
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="m-0 p-0">
+      <h1>hello</h1>
+    </main>
+  )
+}
