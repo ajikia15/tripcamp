a4e4f46d5e0c59eec91d06de96a5a3d35e2bb89c
ajikia15 <ajikia15@gmail.com>
2023-05-02 10:53:52 +0400

    Categicons

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
-
+import categIcons from './components/reusable/categIcons'
 export default function Home() {
   return (
     <main className="m-0 p-0">
       <h1>hello</h1>
+      <categIcons/>
     </main>
   )
 }
