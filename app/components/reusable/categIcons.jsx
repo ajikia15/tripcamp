636d591d5a1f681707747d5b2c07b77bc18fd423
ajikia15 <ajikia15@gmail.com>
2023-05-02 19:38:04 +0400

    Categories

--- app/components/reusable/categIcons.jsx
+++ app/components/reusable/categIcons.jsx
@@ -1,3 +1,15 @@
-export default function categIcons() {
-  return <div>aqane</div>;
+import React from "react";
+export default function CategIcons(props) {
+  return (
+    <div>
+      <ul className="flex flex-row flex-wrap nowrap aspect-square">
+        <li className="flex flex-col place-items-center gap-x-8  text-gray-500">
+          <div className="text-gray-500 aspect-square">
+            {props.svg}
+          </div>{" "}
+          <p className="truncate">{props.name}</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
