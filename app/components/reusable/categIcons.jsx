4d586159084ac7a5e0c28ec1a8322196afd73f63
AniMerabishvili <anni.merabishvili@gmail.com>
2023-05-02 20:56:47 +0400

    Slider-buttons

--- app/components/reusable/categIcons.jsx
+++ app/components/reusable/categIcons.jsx
@@ -2,11 +2,11 @@ import React from "react";
 export default function CategIcons(props) {
   return (
     <div>
-      <ul className="flex flex-row flex-wrap nowrap aspect-square">
-        <li className="flex flex-col place-items-center gap-x-8  text-gray-500">
+      <ul className="flex flex-row flex-wrap nowrap">
+        <li className="flex flex-col place-items-center gap-x-8 text-gray-500">
           <div className="text-gray-500 aspect-square">
             {props.svg}
-          </div>{" "}
+          </div>
           <p className="truncate">{props.name}</p>
         </li>
       </ul>
