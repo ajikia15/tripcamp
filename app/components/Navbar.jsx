59bde493a693ca545cce314f99838c589883074a
aleqsandreqavtaradze <leqsokavtaradze00@gmail.com>
2023-05-02 10:21:37 +0400

    navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,30 @@
 export default function Navbar() {
   return (
-    <div>
-      <h1>aqane navigacia</h1>
-    </div>
+    <div className= "w-full py-2 rounded-full shadow-sm cursor-pointer border-4rem md:w-auto hover:shadow-md transation"> 
+
+ <div className="pl-6 font-semibold">TripCamp</div>
+  <div className="flex flex-row justify-between item-center"
+  >
+
+    <div className="px-6 text-sm font-semibold">
+      when
+      <p>anytime</p>
+      </div>
+      <div className="flex-1 hidden px-6 text-sm font-semibold text-center border-x-[1px] sm:block">
+        price
+        <p>any amount</p>
+      </div>
+      <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-sm font-semibold">
+        <div className="hidden sm:block">
+          who
+          <p>2 guests</p>
+        </div>
+        <div className="p-2 text-white bg-blue-700 rounded-full">
+
+        </div>
+
+      </div>
+  </div>
+  </div>
   );
 }
