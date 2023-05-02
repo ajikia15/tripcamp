790daeafd5ba637815832f8f7efd5c67d12a0357
ajikia15 <ajikia15@gmail.com>
2023-05-02 19:37:21 +0400

    Footer 50%

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,3 +1,35 @@
 export default function Footer() {
-  return <div>footeri aqane</div>;
+  return (
+    <div className="grid grid-cols-1 px-32 bg-gray-100 gap-y-10 md:grid-cols-4 py-14">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Supporting people with disabilities</p>
+        <p>Cancellation options</p>
+        <p>Our Covid-19 Response</p>
+        <p>Report a neighborhood concern</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>Disaster relief housing</p>
+        <p>Combinating discrimination</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>tripmcamp your home</p>
+        <p>tripcover for hosts</p>
+        <p>Explore hosting resources</p>
+        <p>Visit our community forum</p>
+        <p>how to host responsibly</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">TripCamp</h5>
+        <p>Newsroom</p>
+        <p>learn about features</p>
+        <p>Careers</p>
+        <p>Investors</p>
+        <p>Gift cards</p>
+      </div>
+    </div>
+  );
 }
