bbe2f2abed4a43983ae1220b96abefc511485a44
ajikia15 <ajikia15@gmail.com>
2023-05-05 14:30:34 +0400

    Reusable Modal

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Card from './components/reusable/Card'
 import Modal from './components/modals/Modal'
+import ChangeCurrency from './components/modals/ChangeCurrency'
 export default function Home() {
   return (
     <main className="p-0 m-0">
