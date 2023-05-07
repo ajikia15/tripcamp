cd857828ef7483164d06e0f0bb74f40c4178e3f7
ajikia15 <ajikia15@gmail.com>
2023-05-07 09:10:09 +0400

    Switch to server Functions

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
+"use server"
 import Image from 'next/image'
 import Card from './components/reusable/Card'
 import Modal from './components/modals/Modal'
 import ChangeCurrency from './components/modals/ChangeCurrency'
 import Listings from './components/Listings'
+import TempListings from './components/TempListings'
 
-export default function Home() {
+export default async function Home() {
 
   return (
     <main className="p-0 m-0">
       <Listings />
-      <div>
-        <Modal />
-      </div>
-
+      {/* DAAKOMENTARE AM XAZIS QVEMOT TU APIREB FIREBASEDAN WAMOGHEBAS LISTINGEBIS */}
+      <TempListings />
     </main >
   )
 }
