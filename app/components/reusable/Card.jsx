b2bfdd5b1db90938d4e147bcf70f41113ef0b644
ajikia15 <ajikia15@gmail.com>
2023-05-09 11:45:19 +0400

    ability to select slide manually

--- app/components/reusable/Card.jsx
+++ app/components/reusable/Card.jsx
@@ -6,70 +6,91 @@ import React, { useEffect } from "react";
 export default function Card({ children: slides }) {
   const [curr, setCurr] = useState(0);
 
-  const prev = () =>
+  const prev = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     setCurr((curr) =>
       curr === 0 ? slides.length - 1 : curr - 1
     );
-  const next = () =>
+  };
+  const next = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
     setCurr((curr) =>
       curr === slides.length - 1 ? 0 : curr + 1
     );
+  };
+  const setSlider = (event, i) => {
+    event.preventDefault();
 
+    event.stopPropagation();
+    setCurr((curr) => (curr = i));
+  };
   return (
     <div className="relative overflow-hidden rounded-md">
-      {/* <a href="listings/1"> */}
-      <div
-        className="flex object-contain transition-transform duration-1000 ease-out"
-        style={{
-          transform: `translateX(-${curr * 100}%)`,
-        }}>
-        {slides}
-      </div>
-      <div className="absolute inset-0 flex items-center justify-between p-4 text-white opacity-0 hover:opacity-100 hover:transition hover:duration-500 hover:ease-in-out">
-        <button
-          onClick={prev}
-          className="h-0 shadow opacity-80 hover:opacity-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24">
-            <path
-              fill="currentColor"
-              d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
-            />
-          </svg>
-        </button>
-        <button
-          onClick={next}
-          className="h-0 shadow opacity-80 hover:opacity-100">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="32"
-            height="32"
-            viewBox="0 0 24 24">
-            <g transform="translate(24 0) scale(-1 1)">
-              <path
-                fill="currentColor"
-                d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
-              />
-            </g>
-          </svg>
-        </button>
-      </div>
-      <div className="absolute left-0 right-0 bottom-4">
-        <div className="flex items-center justify-center gap-2">
-          {slides.map((_, i) => (
-            <div
-              className={`
-                  transition-all w-1 h-1 aspect-square bg-white rounded-full
-                  ${curr === i ? "w-2 h-2" : "bg-opacity-50"}
-              `}
-            />
-          ))}
+      <a href="listings/1">
+        <div
+          className="flex object-contain transition-transform duration-1000 ease-out"
+          style={{
+            transform: `translateX(-${curr * 100}%)`,
+          }}>
+          {slides}
         </div>
-      </div>
-      {/* </a> */}
+        <div className="absolute inset-0 flex items-center justify-between p-4 text-white opacity-0 hover:opacity-100 hover:transition hover:duration-500 hover:ease-in-out">
+          <button
+            onClick={(event) => prev(event)}
+            className="h-0 transition-all shadow opacity-80 hover:opacity-100">
+            <div className="p-1 bg-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                className="text-gray-600">
+                <path
+                  fill="currentColor"
+                  d="M15.125 21.1L6.7 12.7q-.15-.15-.213-.325T6.425 12q0-.2.062-.375T6.7 11.3l8.425-8.425q.35-.35.875-.35t.9.375q.375.375.375.875t-.375.875L9.55 12l7.35 7.35q.35.35.35.863t-.375.887q-.375.375-.875.375t-.875-.375Z"
+                />
+              </svg>
+            </div>
+          </button>
+          <button
+            onClick={(event) => next(event)}
+            className="h-0 transition-all shadow opacity-80 hover:opacity-100">
+            <div className="p-1 bg-white rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="text-gray-600"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M7.15 21.1q-.375-.375-.375-.888t.375-.887L14.475 12l-7.35-7.35q-.35-.35-.35-.875t.375-.9q.375-.375.888-.375t.887.375l8.4 8.425q.15.15.213.325T17.6 12q0 .2-.063.375t-.212.325L8.9 21.125q-.35.35-.863.35T7.15 21.1Z"
+                />
+              </svg>
+            </div>
+          </button>
+        </div>
+        <div className="absolute left-0 right-0 bottom-4">
+          <div className="flex items-center justify-center gap-1">
+            {slides.map((_, i) => (
+              <div
+                key={i}
+                onClick={(event) => setSlider(event, i)}
+                className={`
+                  transition-all aspect-square bg-white rounded-full opacity-100
+                  ${
+                    curr === i
+                      ? "w-2 h-2 "
+                      : "w-1.5 h-1.5 opacity-[0.8] "
+                  }
+              `}
+              />
+            ))}
+          </div>
+        </div>
+      </a>
     </div>
   );
 }
