cd645995edc10b11ca49bf015d801c0118a60699
ajikia15 <ajikia15@gmail.com>
2023-05-09 11:00:57 +0400

    Fix for slider dots

--- app/components/TempListings.jsx
+++ app/components/TempListings.jsx
@@ -1,82 +1,27 @@
-// "use server";
-// import Card from "./reusable/Card";
-// export default async function TempListings() {
-//   return (
-//     <div className="grid w-full place-items-center">
-//       <div className="grid w-11/12 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:w-4/5 xl:w-5/6 ">
-//         {[...Array(5)].map((_, index) => (
-//           <Card key={index} />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// const slides = [
-//   "https://images.unsplash.com/photo-1565008576549-57569a49371d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1258&q=80",
-//   "https://images.unsplash.com/photo-1549466785-f5c1771646cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
-  
-// ]
-
-
-
 import Card from "./reusable/Card";
 import Page from "../listings/1/page";
 const slides = [
   "https://images.unsplash.com/photo-1565008576549-57569a49371d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1258&q=80",
   "https://images.unsplash.com/photo-1549466785-f5c1771646cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
   "https://images.unsplash.com/photo-1596712407493-9dcc39702033?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-]
+];
 
-
-
-export default function TempListings () {
-return (
-  <div className="grid w-full place-items-center mb-6">
-       <div className="grid w-11/12 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:w-4/5 xl:w-5/6 ">
-         {[...Array(8)].map((_, index) => (
-           <main className="TempListings">
-           <div className="max-w-lg">
-             <Card autoSlide={true} key={index}>
-               {slides.map((s) => (
-                   <img src={s} />
-               ))}
-             </Card>
-           </div>
-         </main>
-         ))}
-       </div>
-</div>
-)
-
+export default function TempListings() {
+  return (
+    <div className="grid w-full mb-6 place-items-center">
+      <div className="grid w-11/12 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:w-4/5 xl:w-5/6 ">
+        {[...Array(8)].map((_, index) => (
+          <main className="TempListings">
+            <div className="max-w-lg">
+              <Card key={index}>
+                {slides.map((s) => (
+                  <img src={s} />
+                ))}
+              </Card>
+            </div>
+          </main>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-// export default function TempListings() {
-//   // const [curr, setCurr] = useState(0)
-
-//   const prev = () => setCurr((curr) => (curr === 0 ? 2 : curr - 1 ))
-//   const next = () => setCurr((curr) => (curr === 0 ? 2 : curr + 1 ))
-
-//   return (
-//     <main className="TempListings">
-//       <div> 
-//         <Card>
-         
-//             <div className="flex relative items-center overflow-hidden w-1/2 transition-transform ease-out duration-500" style={{transform: `translateX(-${curr * 100}%)`}}>
-//             <svg onClick={prev} className="absolute text-white w-10 h-10" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>
-//               <img
-//                 src="https://images.unsplash.com/photo-1565008576549-57569a49371d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1258&q=80"
-//               />
-//               <img
-//                 src="https://images.unsplash.com/photo-1549466785-f5c1771646cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-//               />
-//               <img
-//                 src="https://images.unsplash.com/photo-1565008576549-57569a49371d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1258&q=80"
-//               />
-//             </div>
-          
-//         </Card>
-//       </div>
-//     </main>
-//   )
-// }
--- app/components/reusable/Card.jsx
+++ app/components/reusable/Card.jsx
@@ -1,51 +1,75 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import React, { useEffect } from 'react';
+import { useState } from "react";
+import React, { useEffect } from "react";
 
-export default function Card ({
-  children: slides,
-  // autoSlide = false,
-  // autoSlideInterval = 3000,
-}) {
-  const [curr, setCurr] = useState(0)
+export default function Card({ children: slides }) {
+  const [curr, setCurr] = useState(0);
 
   const prev = () =>
-    setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1))
+    setCurr((curr) =>
+      curr === 0 ? slides.length - 1 : curr - 1
+    );
   const next = () =>
-    setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1))
+    setCurr((curr) =>
+      curr === slides.length - 1 ? 0 : curr + 1
+    );
 
-    // useEffect(() => {
-    //   if (!autoSlide) return
-    //   const slideInterval = setInterval(next, autoSlideInterval)
-    //   return () => clearInterval(slideInterval)
-    // }, [])
-
-    return (
-    <div className="overflow-hidden relative rounded-md">
+  return (
+    <div className="relative overflow-hidden rounded-md">
       {/* <a href="listings/1"> */}
-        <div className="flex transition-transform ease-out duration-1000 object-contain" style={{transform: `translateX(-${curr * 100}%)`}}>
-          {slides}
+      <div
+        className="flex object-contain transition-transform duration-1000 ease-out"
+        style={{
+          transform: `translateX(-${curr * 100}%)`,
+        }}>
+        {slides}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-between p-4 text-white opacity-0 hover:opacity-100 hover:transition hover:duration-500 hover:ease-in-out">
+        <button
+          onClick={prev}
+          className="h-0 shadow opacity-80 hover:opacity-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24">
+            <path
+              fill="currentColor"
+              d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
+            />
+          </svg>
+        </button>
+        <button
+          onClick={next}
+          className="h-0 shadow opacity-80 hover:opacity-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24">
+            <g transform="translate(24 0) scale(-1 1)">
+              <path
+                fill="currentColor"
+                d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"
+              />
+            </g>
+          </svg>
+        </button>
+      </div>
+      <div className="absolute left-0 right-0 bottom-4">
+        <div className="flex items-center justify-center gap-2">
+          {slides.map((_, i) => (
+            <div
+              className={`
+                  transition-all w-1 h-1 aspect-square bg-white rounded-full
+                  ${curr === i ? "w-2 h-2" : "bg-opacity-50"}
+              `}
+            />
+          ))}
         </div>
-        <div className="opacity-0 hover:opacity-100 hover:transition hover:duration-500 hover:ease-in-out absolute inset-0 text-white flex items-center justify-between p-4">
-          <button onClick={prev} className="h-0 shadow opacity-80 hover:opacity-100">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>
-          </button>
-          <button onClick={next} className="h-0 shadow opacity-80 hover:opacity-100">
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g transform="translate(24 0) scale(-1 1)"><path fill="currentColor" d="M11.8 13H15q.425 0 .713-.288T16 12q0-.425-.288-.713T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7q-.275-.275-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275q.275-.275.275-.7t-.275-.7l-.9-.9Zm.2 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></g></svg>
-          </button>
-        </div>
-        <div className="absolute bottom-4 right-0 left-0">
-          <div className="flex items-center justify-center gap-2">
-            {slides.map((_, i) => (
-              <div className={`
-                  transition-all w-2 h-2 bg-white rounded-full
-                  ${curr===i ? "p-2" : "bg-opacity-50"}
-              `}/>
-            ))}
-          </div>
-        </div>
+      </div>
       {/* </a> */}
     </div>
-    )
+  );
 }
