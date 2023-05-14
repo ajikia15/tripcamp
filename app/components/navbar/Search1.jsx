3ad938e033cc59fa4a929c079d749e4cd6776418
ajikia15 <ajikia15@gmail.com>
2023-05-14 17:08:02 +0400

    Firebase Search

---
+++ app/components/navbar/Search1.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+export default function Search() {
+  const [active, setActive] = useState(false);
+  const inputRef = useRef(null);
+
+  const clickedInside = (e) => {
+    e.preventDefault();
+    setActive(true);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (
+        active &&
+        inputRef.current &&
+        !inputRef.current.contains(e.target)
+      ) {
+        setActive(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener(
+        "click",
+        handleClickOutside
+      );
+    };
+  }, [active, inputRef]);
+
+  return (
+    <>
+      {active ? (
+        <li className="relative flex flex-col">
+          <input
+            className="w-full outline-none"
+            type="text"
+            autoFocus
+            ref={inputRef}
+            placeholder="Where to?"
+          />
+          <ul className="absolute -left-12 right-0 flex flex-col top-[calc(100%+1rem)] bg-white border-black border rounded-xl">
+            <li>balls</li>
+          </ul>
+        </li>
+      ) : (
+        <li
+          className="flex flex-col"
+          onClick={clickedInside}>
+          <h3 className="font-semibold">Where to?</h3>
+          <p className="text-xs text-gray-500">
+            Anywhere • Any Week • Add guests
+          </p>
+        </li>
+      )}
+    </>
+  );
+}
