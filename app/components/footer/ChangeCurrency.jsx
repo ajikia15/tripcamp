b9280d1b65b2b6bb90745b80bcfa4c83d95c87ed
ajikia15 <ajikia15@gmail.com>
2023-05-16 15:07:58 +0400

    Reworked Modals

---
+++ app/components/footer/ChangeCurrency.jsx
@@ -0,0 +1,50 @@
+"use client";
+import "./style.css";
+import { useState } from "react";
+
+const ChangeCurrency = ({ active }) => {
+  return (
+    <>
+      <div className="cursor-pointer">$ USD</div>
+      {active && (
+        <div className="absolute p-4 text-lg bg-white right-52 bottom-11 rounded-2xl">
+          <ul className="flex flex-col">
+            <li className="flex flex-row items-center justify-between p-4 border-b border-gray-300 cursor-pointer gap-x-20">
+              <p className="font-semibold">
+                $&nbsp;&nbsp;USD
+              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 32 32">
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  stroke-width="2"
+                  d="m6.5 17l6 6l13-13"
+                />
+              </svg>
+            </li>
+            <li className="flex flex-row items-center justify-between p-4 border-b border-gray-300 cursor-pointer gap-x-20">
+              <p className="font-semibold">
+                ₾&nbsp;&nbsp;GEL
+              </p>
+              <p></p>
+            </li>
+            <li className="flex flex-row items-center justify-between p-4 pb-3 border-gray-300 cursor-pointer gap-x-20">
+              <p className="font-semibold">
+                €&nbsp;&nbsp;EURO
+              </p>
+              <p></p>
+            </li>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ChangeCurrency;
