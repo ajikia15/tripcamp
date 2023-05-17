5790167bb771f248d5baa2f17905582525b0a4e8
aleqsandreqavtaradze <leqsokavtaradze00@gmail.com>
2023-05-17 11:08:56 +0400

    Working Map

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <body className={inter.className}>
-
+          <div id="map"></div>
         <Navbar />
         <Categories />
         {children}
