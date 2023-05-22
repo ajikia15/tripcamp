dc9964ac336f3a60c3d9703763efedd477278ab4
Ani Merabishvili <anni.merabishvili@gmail.com>
2023-05-22 10:42:17 +0400

    Slider

--- app/listings/[id]/Slaidera.jsx
+++ app/listings/[id]/Slaidera.jsx
@@ -1,9 +1,24 @@
-const Slaidera = () => {
+import Carousel from "./NewCarousel";
+import Image from "next/image";
+export default function Slidera() {
+  const images = [
+    "https://images.unsplash.com/photo-1464219789935-c2d9d9aba644?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    "https://images.unsplash.com/photo-1561361513-2d000a50f0dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80",
+    "https://images.unsplash.com/photo-1461435218581-ff0972867e90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80",
+  ];
+  
   return (
-    <div className="my-12">
-      <div>aqane</div>
+    <div className="lg:w-3/4 mx-auto my-2">
+      <Carousel loop>
+        {images.map((src, i) => {
+          return (
+            <div className="relative h-64 flex-[0_0_100%]" key={i}>
+              <Image src={src} fill className="object-cover" alt="alt" />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
 
-export default Slaidera;
