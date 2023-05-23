46c33ff47ec8a5ac643f01ad6c94063fbe57426e
ajikia15 <ajikia15@gmail.com>
2023-05-23 13:58:47 +0400

    Algorithmic sort of the posts

--- app/map/page.js
+++ app/map/page.js
@@ -1,24 +1,29 @@
 "use client"
-import {
-    MapContainer,
-    Marker,
-    Popup,
-    TileLayer,
-} from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 import { useState, useEffect, useRef } from "react";
-import L from "leaflet";
 import { db } from "../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 import Link from "next/link";
 import "../globals.css"
 import Image from "next/image";
 
+import {
+    MapContainer,
+    Marker,
+    Popup,
+    TileLayer,
+} from "react-leaflet";
+import L from "leaflet";
+
 const MainMap = () => {
+
+    const [isBrowser, setIsBrowser] = useState(false);
     const [houses, setHouses] = useState([]);
     const housesCollectionRef = collection(db, "Houses");
     const [position, setPosition] = useState([]);
     useEffect(() => {
+        setIsBrowser(true);
         const getHouses = async () => {
             const data = await getDocs(housesCollectionRef);
             setHouses(
@@ -32,12 +37,18 @@ const MainMap = () => {
         getHouses();
     }, []);
 
+
+
+
     useEffect(() => {
         if (houses.length > 0 && houses[0].Position) {
             const { Latit, Longi } = houses[0].Position;
             setPosition([Latit, Longi]);
         }
     }, [houses]);
+    if (!isBrowser) {
+        return null;
+    }
     return (
         <>
             <div className="fixed z-20 -translate-x-1/2 cursor-pointer bottom-12 left-1/2">
@@ -54,12 +65,13 @@ const MainMap = () => {
                 zoom={13}
                 style={{ height: "60vh" }}>
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://carto.com/" target="_blank">Carto</a> | Map data &copy; <a href="https://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors'
                 />
 
                 {houses.map((house) => (
                     <Marker
-                        key={house.name}
+                        key={house.id}
                         position={[
                             house.Position.Latit,
                             house.Position.Longi,
@@ -69,17 +81,19 @@ const MainMap = () => {
                             html: `<div>$${house.Price}</div>`,
                         })}>
                         <Popup>
-                            <div className="flex flex-col gap-y-2">
-                                <div className="relative w-full rounded-md aspect-square">
-                                    <Image src={house.Photo[0]} fill={true} />
+                            <Link href={`/listings/${house.id}`}>
+                                <div className="flex flex-col gap-y-2">
+                                    <div className="relative w-full rounded-md aspect-square">
+                                        <Image src={house.Photo[0]} fill={true} />
+                                    </div>
+                                    <p className="text-lg font-bold">{house.Name}</p>
+                                    <p className="text-md text-zinc-500">{house.Address.split("~").join(" ")}</p>
+                                    <div className="flex items-center gap-x-2">
+                                        <p className="font-bold">{house.Price}₾</p>
+                                        <p className="text-md">Night</p>
+                                    </div>
                                 </div>
-                                <p className="text-lg font-bold">{house.Name}</p>
-                                <p className="text-md text-zinc-500">{house.Address.split("~").join(" ")}</p>
-                                <div className="flex items-center gap-x-2">
-                                    <p className="font-bold">{house.Price}₾</p>
-                                    <p className="text-md">Night</p>
-                                </div>
-                            </div>
+                            </Link>
                         </Popup>
                     </Marker>
                 ))}
