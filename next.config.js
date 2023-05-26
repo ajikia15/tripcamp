2e1101ad8f92fbdfbe99dab334aa5c08ea5033fe
ajikia15 <ajikia15@gmail.com>
2023-05-26 16:05:06 +0400

    environment variable setup

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
     domains: ["images.unsplash.com", "firebasestorage.googleapis.com", "i0.wp.com", "simlitbangdiklat.kemenag.go.id"],
 
   },
+  env: {
+    APIKEY: process.env.APIKEY,
+    AUTHDOMAIN: process.env.AUTHDOMAIN,
+
+    DATABASEURL: process.env.DATABASEURL,
+    PROJECTID: process.env.PROJECTID,
+    STORAGEBUCKET: process.env.STORAGEBUCKET,
+    MESSAGINGSENDERID: process.env.MESSAGINGSENDERID,
+    APPID: process.env.APPID,
+    MEASUREMENTID: process.env.MEASUREMENTID,
+
+  },
 }
 
 module.exports = nextConfig
