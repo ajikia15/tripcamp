0e1153580eb3846ea614ae68bdae333e75ee963a
ajikia15 <aleksandrej15@gmail.com>
2023-10-21 13:32:56 +0400

    test10

--- app/map/page.js
+++ app/map/page.js
@@ -20,7 +20,7 @@ const MainMap = () => {
   const [filteredHouses, setFilteredHouses] = useState([]);
   useEffect(() => {
     if (houses.length < 1) {
-      console.alert("length is less than 1 of houses, refetching houses");
+      alert("length is less than 1 of houses, refetching houses");
       setLoading(true);
       const getHouses = async () => {
         const housesCollectionRef = collection(db, "Houses");
@@ -43,11 +43,11 @@ const MainMap = () => {
         setLoading(false);
       };
       getHouses();
-      console.alert("fetched houses");
+      alert("fetched houses");
     }
-  }, [houses]); // fix for no content after refreshing map
+  }, []); // fix for no content after refreshing map
   useEffect(() => {
-    console.alert("filtering houses");
+    alert("filtering houses");
 
     setFilteredHouses(
       houses.filter((house) => {
@@ -87,8 +87,8 @@ const MainMap = () => {
         );
       })
     );
-    console.alert("filtering done");
-  }, [houseId, guestsAmount, minMax, filterTerm, searchTerm]);
+    alert("filtering done");
+  }, [houses, houseId, guestsAmount, minMax, filterTerm, searchTerm]);
   const Map = dynamic(() => import("./Map"), {
     ssr: false,
   });
